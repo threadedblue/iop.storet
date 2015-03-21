6b7c20a65d5448dd2da69c8045185cd1d8191264
gcr <geoffry.roberts@gmail.com>
2015-03-20 20:39:09 -0400

    commit

--- src/main/java/iop/ccd2d4m/marshaler/TedgeMarshaler.java
+++
@@ -1,9 +0,0 @@
-package iop.storet.marshaler;
-
-public interface TedgeMarshaler {
-	
-	public static final String ACCOUNT_NO_FORMAT = "0000";
-
-	public abstract void build();
-
-}
--- src/test/java/iop/ccd2d4m/marshaler/impl/CDAFilter.java
+++
@@ -1,9 +0,0 @@
-package iop.storet.marshaler.impl;
-import java.io.File;
-import java.io.FileFilter;
-
-public class CDAFilter implements FileFilter {
-	public boolean accept(File pathname) {
-		return (pathname.getName().endsWith("xml") || pathname.isDirectory());
-	}
-}
