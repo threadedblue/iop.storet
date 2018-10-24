d2d10ae0b8066d89777f7e0532a43febae9545ec
threadedblue <threaded@gmail.com>
2018-10-24 11:02:39 -0400

    Configuration changes

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DIR=`pwd`
-NAME="xml2xpath"
+NAME="ccd.bfs"
 cd $NAME
-time java -jar $NAME-0.0.1.jar -i /ccd/ccd1 -t ccddd -fs hdfs://haz00:9000 -l accumulo -zk haz00:2181 -ow -r
+time java -jar $NAME-0.0.1.jar -i /ccd -o ccdRT -fs hdfs://haz00:9000 -l accumulo -zk haz00:2181 -ow
 cd $DIR
