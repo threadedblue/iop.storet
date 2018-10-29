3cc0bf0010842c9c37bec0a8d23ffb121fae69d0
threadedblue <threaded@gmail.com>
2018-10-29 16:17:49 -0400

    Fixed no reduce peoblem

--- run.sh
+++ run.sh
@@ -2,6 +2,24 @@
 
 DIR=`pwd`
 NAME="xml2xpath"
+STEM="ccd4"
+
+COUNT="500"
 cd $NAME
-time java -jar $NAME-0.0.1.jar -i /ccd -o /ccdxpath -r -ow
+time java -jar $NAME-0.0.1.jar -i "/ccd500/$STEM/$COUNT" -o "/ccdxpath/$STEM/$COUNT" -r -ow
+cd $DIR
+COUNT="1000"
+
+cd $NAME
+time java -jar $NAME-0.0.1.jar -i "/ccd500/$STEM/$COUNT" -o "/ccdxpath/$STEM/$COUNT" -r -ow
+cd $DIR
+
+COUNT="1500"
+cd $NAME
+time java -jar $NAME-0.0.1.jar -i "/ccd500/$STEM/$COUNT" -o "/ccdxpath/$STEM/$COUNT" -r -ow
+cd $DIR
+
+COUNT="2000"
+cd $NAME
+time java -jar $NAME-0.0.1.jar -i "/ccd500/$STEM/$COUNT" -o "/ccdxpath/$STEM/$COUNT" -r -ow
 cd $DIR
