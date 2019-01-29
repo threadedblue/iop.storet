3eeaf1cd28ed77bfeff98e6391f8f5cd0dfd63ba
threadedblue <threaded@gmail.com>
2019-01-29 12:43:36 -0500

    Add input record counter

--- run.sh
+++ run.sh
@@ -2,28 +2,28 @@
 
 DIR=`pwd`
 NAME="xml2xpath"
-STEM="ccd0"
+STEM="ccd1"
 
-cd $NAME
-time java -jar $NAME-0.0.1.jar -i "/$STEM" -o "/ccdxpath/$STEM" -r -ow
-cd $DIR
+#cd $NAME
+#time java -jar $NAME-0.0.1.jar -i "/$STEM" -o "/ccdxpath/$STEM" -r -ow
+#cd $DIR
 
 COUNT="500"
 cd $NAME
-#time java -jar $NAME-0.0.1.jar -i "/ccd500/$STEM/$COUNT" -o "/ccdxpath/$STEM/$COUNT" -r -ow
+time java -jar $NAME-0.0.1.jar -i "/ccd500/$STEM/$COUNT" -o "/ccdxpath/$STEM/$COUNT" -r -ow
 cd $DIR
 COUNT="1000"
 
 cd $NAME
-#time java -jar $NAME-0.0.1.jar -i "/ccd500/$STEM/$COUNT" -o "/ccdxpath/$STEM/$COUNT" -r -ow
+time java -jar $NAME-0.0.1.jar -i "/ccd500/$STEM/$COUNT" -o "/ccdxpath/$STEM/$COUNT" -r -ow
 cd $DIR
 
 COUNT="1500"
 cd $NAME
-#time java -jar $NAME-0.0.1.jar -i "/ccd500/$STEM/$COUNT" -o "/ccdxpath/$STEM/$COUNT" -r -ow
+time java -jar $NAME-0.0.1.jar -i "/ccd500/$STEM/$COUNT" -o "/ccdxpath/$STEM/$COUNT" -r -ow
 cd $DIR
 
 COUNT="2000"
 cd $NAME
-#time java -jar $NAME-0.0.1.jar -i "/ccd500/$STEM/$COUNT" -o "/ccdxpath/$STEM/$COUNT" -r -ow
+time java -jar $NAME-0.0.1.jar -i "/ccd500/$STEM/$COUNT" -o "/ccdxpath/$STEM/$COUNT" -r -ow
 cd $DIR
